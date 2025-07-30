286ae6778dc28f77e83c5848c50e770cc4d2d3b6
ToniR90 <romy90.t@gmail.com>
2025-07-30 12:31:43 +0200

    delete: Sprint1_T1_Ex1_lvl1 unnecessary anotations

--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level1_exercice1/src/main/java/org/example/model/Instrument.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level1_exercice1/src/main/java/org/example/model/Instrument.java
@@ -2,13 +2,11 @@ package org.example.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
 @AllArgsConstructor
-@NoArgsConstructor
 public abstract class Instrument {
     private String name;
     private double price;
