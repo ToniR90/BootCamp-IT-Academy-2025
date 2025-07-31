700d4ecc6504abf783c19d7e9dd6679b3a192ab3
ToniR90 <romy90.t@gmail.com>
2025-07-31 12:51:15 +0200

    feat: abstract methods in New model added

--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/model/FootballNew.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/model/FootballNew.java
@@ -1,5 +1,25 @@
 package org.example.model;
 
-public class FootballNew extends New {
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.example.model.enums.FootballClub;
+import org.example.model.enums.FootballCompetition;
+import org.example.model.enums.FootballPlayer;
 
+@Data
+@SuperBuilder
+public class FootballNew extends New {
+    private FootballCompetition competition;
+    private FootballClub club;
+    private FootballPlayer player;
+
+    @Override
+    protected String calculatePrice() {
+        return "";
+    }
+
+    @Override
+    protected String calculatePunctuation() {
+        return "";
+    }
 }
--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/model/New.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/model/New.java
@@ -1,11 +1,18 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
+@AllArgsConstructor
 public abstract class New {
     private String headline;
     private String text = "";
     private int punctuation;
     private double price;
+
+    protected abstract String calculatePrice();
+    protected abstract String calculatePunctuation();
 }
