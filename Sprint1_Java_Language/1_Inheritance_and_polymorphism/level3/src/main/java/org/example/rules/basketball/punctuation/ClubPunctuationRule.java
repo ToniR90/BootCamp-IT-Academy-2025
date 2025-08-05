f0e2dbef0721075261009c19c75d95d2f3362895
ToniR90 <romy90.t@gmail.com>
2025-08-05 09:21:51 +0200

    feat: punctuation Basketball news added

--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/rules/basketball/punctuation/ClubPunctuationRule.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/rules/basketball/punctuation/ClubPunctuationRule.java
@@ -1,4 +1,24 @@
 package org.example.rules.basketball.punctuation;
 
-public class ClubPunctuationRule {
+import org.example.model.BasketballNew;
+import org.example.model.New;
+import org.example.model.enums.BasketballClub;
+import org.example.rules.PunctuationRule;
+
+import java.util.Set;
+
+public class ClubPunctuationRule implements PunctuationRule {
+
+    private static final Set<BasketballClub> keyClubs = Set.of(
+            BasketballClub.MADRID,
+            BasketballClub.BARCELONA
+    );
+
+    @Override
+    public int apply(New newItem) {
+        if(!(newItem instanceof BasketballNew basketballNew)) {
+            return 0;
+        }
+        return keyClubs.contains(basketballNew.getBasketballClub()) ? 1 : 0;
+    }
 }
--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/rules/basketball/punctuation/CompetitionPunctuationRule.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/rules/basketball/punctuation/CompetitionPunctuationRule.java
@@ -1,4 +1,26 @@
 package org.example.rules.basketball.punctuation;
 
-public class CompetitionPunctuationRule {
+import org.example.model.BasketballNew;
+import org.example.model.New;
+import org.example.model.enums.BasketballCompetition;
+import org.example.rules.PunctuationRule;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CompetitionPunctuationRule implements PunctuationRule {
+
+    private static final Map<BasketballCompetition , Integer> keyCompetitions = Map.of(
+            BasketballCompetition.EURO_LEAGUE, 3 ,
+            BasketballCompetition.ACB , 2
+    );
+
+    @Override
+    public int apply(New newItem) {
+        if(!(newItem instanceof BasketballNew basketballNew)) {
+            return 0;
+        }
+        return keyCompetitions.getOrDefault(basketballNew.getBasketballCompetition() , 0);
+    }
 }
