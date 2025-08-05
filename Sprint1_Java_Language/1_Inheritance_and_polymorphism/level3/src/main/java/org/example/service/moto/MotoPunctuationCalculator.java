7bc5cf65f8697af7bfc8ac3db1d171e23e0bc16c
ToniR90 <romy90.t@gmail.com>
2025-08-05 15:11:56 +0200

    feat: moto services added

---
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/service/moto/MotoPunctuationCalculator.java
@@ -0,0 +1,21 @@
+package org.example.service.moto;
+
+import lombok.AllArgsConstructor;
+import org.example.model.New;
+import org.example.rules.PunctuationRule;
+import org.example.service.PunctuationCalculator;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class MotoPunctuationCalculator implements PunctuationCalculator {
+
+    private final List<PunctuationRule> rules;
+
+    @Override
+    public int calculate(New newItem) {
+        return 3 + rules.stream()
+                .mapToInt(rule -> rule.apply(newItem))
+                .sum();
+    }
+}
