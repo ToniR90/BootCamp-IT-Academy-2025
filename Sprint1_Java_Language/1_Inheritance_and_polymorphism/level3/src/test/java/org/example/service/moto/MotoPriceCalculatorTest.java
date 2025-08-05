f18089d6a327a7bad99eb73254295729725c4d07
ToniR90 <romy90.t@gmail.com>
2025-08-05 16:28:34 +0200

    feat: moto test

--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/moto/MotoPriceCalculatorTest.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/moto/MotoPriceCalculatorTest.java
@@ -1,12 +1,29 @@
 package org.example.service.moto;
 
+import org.example.model.MotoNew;
+import org.example.model.enums.MotoTeam;
+import org.example.rules.PriceRule;
+import org.example.rules.moto.price.TeamPriceRule;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MotoPriceCalculatorTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateCorrectPrice() {
+        MotoNew motoNew = MotoNew.builder()
+                .motoTeam(MotoTeam.OTHER)
+                .build();
+        List<PriceRule> rules = List.of(
+                new TeamPriceRule()
+        );
+
+        MotoPriceCalculator calculator = new MotoPriceCalculator(rules);
+
+        double result = calculator.calculate(motoNew);
+        assertEquals(100 , result);
     }
 }
--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/moto/MotoPunctuationCalculatorTest.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/moto/MotoPunctuationCalculatorTest.java
@@ -1,12 +1,30 @@
 package org.example.service.moto;
 
+import org.example.model.MotoNew;
+import org.example.model.enums.MotoTeam;
+import org.example.rules.PunctuationRule;
+import org.example.rules.moto.punctuation.TeamPunctuationRule;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MotoPunctuationCalculatorTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateCorrectPunctuation() {
+        MotoNew motoNew = MotoNew.builder()
+                .motoTeam(MotoTeam.OTHER)
+                .build();
+
+        List<PunctuationRule> rules = List.of(
+                new TeamPunctuationRule()
+        );
+
+        MotoPunctuationCalculator calculator = new MotoPunctuationCalculator(rules);
+
+        int result = calculator.calculate(motoNew);
+        assertEquals(3 , result);
     }
 }
