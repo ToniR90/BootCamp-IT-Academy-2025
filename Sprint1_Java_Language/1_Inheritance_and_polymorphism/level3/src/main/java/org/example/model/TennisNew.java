7987d300bcb05fe257ccd97c4440fc0a8d199010
ToniR90 <romy90.t@gmail.com>
2025-08-05 15:13:34 +0200

    feat: tennis model

--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/model/TennisNew.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/main/java/org/example/model/TennisNew.java
@@ -1,4 +1,9 @@
 package org.example.model;
 
+import org.example.model.enums.TennisCompetition;
+import org.example.model.enums.TennisPlayer;
+
 public class TennisNew extends New{
+    private TennisCompetition tennisCompetition;
+    private TennisPlayer tennisPlayer;
 }
