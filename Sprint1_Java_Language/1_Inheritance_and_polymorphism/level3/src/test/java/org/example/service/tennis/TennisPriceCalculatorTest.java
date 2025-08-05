56dd2d99292660cf2041a915aec11bcc8d5fce26
ToniR90 <romy90.t@gmail.com>
2025-08-05 16:33:49 +0200

    feat: Tennis test

--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/tennis/TennisPriceCalculatorTest.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/tennis/TennisPriceCalculatorTest.java
@@ -1,12 +1,30 @@
 package org.example.service.tennis;
 
+import org.example.model.TennisNew;
+import org.example.model.enums.TennisPlayer;
+import org.example.rules.PriceRule;
+import org.example.rules.tennis.price.TennisPlayerPriceRule;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TennisPriceCalculatorTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateCorrectPrice() {
+        TennisNew tennisNew = TennisNew.builder()
+                .tennisPlayer(TennisPlayer.FEDERER)
+                .build();
+
+        List<PriceRule> rules = List.of(
+                new TennisPlayerPriceRule()
+        );
+
+        TennisPriceCalculator calculator = new TennisPriceCalculator(rules);
+
+        double result = calculator.calculate(tennisNew);
+        assertEquals(250 , result);
     }
 }
--- Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/tennis/TennisPunctuationCalculatorTest.java
+++ Sprint1_Java_Language/1_Inheritance_and_polymorphism/level3/src/test/java/org/example/service/tennis/TennisPunctuationCalculatorTest.java
@@ -1,12 +1,30 @@
 package org.example.service.tennis;
 
+import org.example.model.TennisNew;
+import org.example.model.enums.TennisPlayer;
+import org.example.rules.PunctuationRule;
+import org.example.rules.tennis.punctuation.TennisPlayerPunctuationRule;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TennisPunctuationCalculatorTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateCorrectPunctuation() {
+        TennisNew tennisNew = TennisNew.builder()
+                .tennisPlayer(TennisPlayer.FEDERER)
+                .build();
+
+        List<PunctuationRule> rules = List.of(
+                new TennisPlayerPunctuationRule()
+        );
+
+        TennisPunctuationCalculator calculator = new TennisPunctuationCalculator(rules);
+
+        int result = calculator.calculate(tennisNew);
+        assertEquals(7 , result);
     }
 }
